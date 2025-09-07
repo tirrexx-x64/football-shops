1e3650b7c472ae2a794fc566a8f2766b9d0ee188
tirrexx-x64 <tirtarendysi77@gmail.com>
2025-09-07 12:47:17 +0700

    update

--- football_shops/settings.py
+++ football_shops/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv(
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
 # Debug mode → otomatis off kalau PRODUCTION=True
-DEBUG = not PRODUCTION
+DEBUG = FALSE
 
 # Hosts yang diizinkan
 ALLOWED_HOSTS = [
